a230b75882e7b213e46d63b5fabd71708d138983
ahesh <aheshalagu@gmail.com>
2024-02-13 12:11:20 +0530

    added class HttpHandler

---
+++ src/main/java/io/jenkins/plugins/http/HttpHandler.java
@@ -0,0 +1,23 @@
+ package io.jenkins.plugins.http;
+
+ import hudson.model.RootAction;
+
+
+ public class HttpHandler implements RootAction {
+
+    @Override
+    public String getIconFileName() {
+        return null;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return null;
+    }
+
+    @Override
+    public String getUrlName() {
+        return "custom-api";
+    }
+
+ }
