14e4c1dcd3a368c2b0f0a4c84a51ecea43c0e74a
ahesh <aheshalagu@gmail.com>
2024-02-13 17:16:23 +0530

    added a Http get method which accepts a parameter

--- src/main/java/io/jenkins/plugins/http/HttpHandler.java
+++ src/main/java/io/jenkins/plugins/http/HttpHandler.java
@@ -1,13 +1,14 @@
  package io.jenkins.plugins.http;
 
+ import static java.util.Objects.requireNonNull;
  import hudson.Extension;
  import hudson.model.RootAction;
  import io.jenkins.plugins.model.MyJsonObject;
  import net.sf.json.JSONObject;
+ import org.kohsuke.stapler.QueryParameter;
  import org.kohsuke.stapler.WebMethod;
  import org.kohsuke.stapler.json.JsonHttpResponse;
  import org.kohsuke.stapler.verb.GET;
- import org.springframework.lang.NonNull;
  import edu.umd.cs.findbugs.annotations.CheckForNull;
 
  @Extension
@@ -34,6 +35,15 @@
     @WebMethod(name="get-example")
      public JsonHttpResponse getExample() {
         JSONObject response = JSONObject.fromObject(new MyJsonObject("I am jenkins", 10));
+        return new JsonHttpResponse(response, 200);
+    }
+
+    @GET
+    @WebMethod(name="get-example-param")
+    public JsonHttpResponse getWithParams(@QueryParameter(required = true) String paramValue){
+        requireNonNull(paramValue);
+        MyJsonObject myJsonObject = new MyJsonObject("I am jenkins" + paramValue);
+        JSONObject response = JSONObject.fromObject(myJsonObject);
         return new JsonHttpResponse(response, 200);
     }
  }
